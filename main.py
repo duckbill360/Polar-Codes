1e56589a9a83bf1131828fbea4f9f1a7a36689e8
Po-Wei Cheng <a821023a821023@yahoo.com.tw>
2017-08-01 14:23:31 +0800

    .

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # Polar Codes with BP Decoding
-#
 
 import polar_codes
 import numpy as np
@@ -8,13 +7,23 @@ import numpy as np
 print("------------------ENCODING------------------")
 
 message = np.array([1, 0, 1, 0, 1, 0, 1, 0], dtype=np.float64)
+N = message.size    # code length
+
 # G is the generator matrix and the size is specified by the message length.
-G = polar_codes.generate_G_N(message.size)
+G = polar_codes.generate_G_N(N)
+
+# Show the generator matrix.
 print('Generator Matrix:\n', G)
 codeword = polar_codes.encode_message(message)
 
+# Show the message and the corresponding codeword.
 print('Message: \n', message)
 print('Codeword:\n', codeword)
+
+
+################# CHANNEL ################
+
+
 
 
 ################ DECODING ################
